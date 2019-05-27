71539a103619a21b4c80b6f05ba30161592c2641
jackyk <jackykimani13@gmail.com>
2019-05-27 15:17:37 +0000

    Price api added

--- prices/src/App.js
+++ prices/src/App.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react'
 import './App.css'
-// import logo from './logo.svg'
-import PriceForm from './ui/PriceForm'
+
+import Price from './ui/Price'
+// import priceInfo from './ui/priceInfo';
 
 
 export class App extends Component {
+
+  state={
+    month:undefined,
+    year:undefined
+  }
+
+getPrice = async(e)=>{
+
+  e.preventDefault()
+    
+    const month = e.target.elements.month.value
+    const year = e.target.elements.year.value
+
+  const apiCall = await fetch(`https://yfarmapi.herokuapp.com/price/v1?month=${month}&year=${year}`)
+  const data = await apiCall.json()
+  if(month && year){
+    this.setState({
+      month:data.month,
+      year:data.year
+    })
+  
+  }else{
+    this.setState({
+      month:'',
+      year:''
+    })
+  }
+
+}
+
+
+
   render() {
     return (
       <div>
-          <PriceForm/>
+        {/* <Price/> */}
+          <Price getPrice = {this.getPrice} />   
+          <priceInfo month= {this.state.month}
+                     year = {this.state.year}/>
       </div>
     )
+
   }
 }
 
